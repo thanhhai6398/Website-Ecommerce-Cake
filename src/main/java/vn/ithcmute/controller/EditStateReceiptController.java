16ffee8257493bed68dcfc407b8ca69dab9b0569
thanhhai6398 <thanhhai6398@gmail.com>
2023-07-19 12:39:13 +0700

    project using Servlets-JSP-Bootstrap-Tomcat

---
+++ src/main/java/vn/ithcmute/controller/EditStateReceiptController.java
@@ -0,0 +1,27 @@
+package vn.ithcmute.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vn.ithcmute.service.ReceiptService;
+import vn.ithcmute.service.impl.ReceiptServiceImpl;
+
+@WebServlet(urlPatterns = { "/editState" })
+public class EditStateReceiptController extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	ReceiptService receiptService = new ReceiptServiceImpl();
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String id = req.getParameter("id");
+		String stId = req.getParameter("stId");
+		
+		receiptService.updateState(Integer.parseInt(id), Integer.parseInt(stId));
+		
+		resp.sendRedirect(req.getContextPath() + "/home");
+	}	
+		
+}
